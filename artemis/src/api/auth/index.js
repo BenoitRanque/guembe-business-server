f237fcf65f9bd0ca6168542aafeef99d964862eb
BenoitRanque <ranque.benoit@gmail.com>
2019-05-29 04:19:12 -0400

    more work on auth... still much to do

--- artemis/src/api/auth/index.js
+++ artemis/src/api/auth/index.js
@@ -6,70 +6,68 @@ const uuid = require('uuid/v4')
 const bodyParser = require('body-parser')
 const { ForbiddenError, NotFoundError } = require('../../utils/errors')
 
-const { Issuer, generators, custom, } = require('openid-client')
-
 const app = express()
 
 app.use(cookieParser())
 
-app.post('/callback', express.urlencoded({ extended: false }), async function (req, res, next) {
+// app.post('/callback', express.urlencoded({ extended: false }), async function (req, res, next) {
 
-  const googleIssuer = await Issuer.discover('https://accounts.google.com') // => Promise
+//   const googleIssuer = await Issuer.discover('https://accounts.google.com') // => Promise
 
-  const client = new googleIssuer.Client({
-    client_id: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
-    redirect_uris: ['http://localhost:9090/api/v1/auth/callback'],
-    response_types: ['id_token'],
-    // id_token_signed_response_alg (default "RS256")
-  })
+//   const client = new googleIssuer.Client({
+//     client_id: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+//     redirect_uris: ['http://localhost:9090/api/v1/auth/callback'],
+//     response_types: ['id_token'],
+//     // id_token_signed_response_alg (default "RS256")
+//   })
 
-  client[custom.clock_tolerance] = 120 // to allow a 2 minute skew
+//   client[custom.clock_tolerance] = 120 // to allow a 2 minute skew
 
-  const nonce = req.cookies['openid-nonce']
-  res.clearCookie('openid-nonce')
-  // throw error if nonce is absent
+//   const nonce = req.cookies['openid-nonce']
+//   res.clearCookie('openid-nonce')
+//   // throw error if nonce is absent
 
-  const params = client.callbackParams(req)
-  const tokenSet = await client.callback(null, params, { nonce })
+//   const params = client.callbackParams(req)
+//   const tokenSet = await client.callback(null, params, { nonce })
 
-  console.log('received and validated tokens %j', tokenSet);
-  console.log('validated ID Token claims %j', tokenSet.claims());
+//   console.log('received and validated tokens %j', tokenSet);
+//   console.log('validated ID Token claims %j', tokenSet.claims());
 
-  res.redirect('http://localhost:8080')
-})
+//   res.redirect('http://localhost:8080')
+// })
 
 
-app.get('/authenticate/:provider', async function (req, res, next) {
-  const googleIssuer = await Issuer.discover('https://accounts.google.com')
+// app.get('/authenticate/:provider', async function (req, res, next) {
+//   const googleIssuer = await Issuer.discover('https://accounts.google.com')
 
-  const client = new googleIssuer.Client({
-    client_id: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
-    redirect_uris: ['http://localhost:9090/api/v1/auth/callback'],
-    response_types: ['id_token'],
-    // id_token_signed_response_alg (default "RS256")
-  })
+//   const client = new googleIssuer.Client({
+//     client_id: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+//     redirect_uris: ['http://localhost:9090/api/v1/auth/callback'],
+//     response_types: ['id_token'],
+//     // id_token_signed_response_alg (default "RS256")
+//   })
 
-  client[custom.clock_tolerance] = 120 // to allow a 2 minute skew
+//   client[custom.clock_tolerance] = 120 // to allow a 2 minute skew
 
-  const nonce = generators.nonce()
-  // store the nonce in your framework's session mechanism, if it is a cookie based solution
-  // it should be httpOnly (not readable by javascript) and encrypted.
-  res.cookie('openid-nonce', nonce, {
-    httpOnly: true,
-    secure: false,
-    maxAge: 5 * 60 * 1000 // 5 minutes
-  })
+//   const nonce = generators.nonce()
+//   // store the nonce in your framework's session mechanism, if it is a cookie based solution
+//   // it should be httpOnly (not readable by javascript) and encrypted.
+//   res.cookie('openid-nonce', nonce, {
+//     httpOnly: true,
+//     secure: false,
+//     maxAge: 5 * 60 * 1000 // 5 minutes
+//   })
 
-  const url = client.authorizationUrl({
-    scope: 'openid email profile',
-    // response_mode: 'id_token',
-    response_mode: 'form_post',
-    nonce
-  })
+//   const url = client.authorizationUrl({
+//     scope: 'openid email profile',
+//     // response_mode: 'id_token',
+//     response_mode: 'form_post',
+//     nonce
+//   })
 
-  res.redirect(url)
-  // res.status(200).json({ url })
-})
+//   res.redirect(url)
+//   // res.status(200).json({ url })
+// })
 
 // app.get('/authenticate/:provider', function (req, res, next) {
 
@@ -102,9 +100,90 @@ app.get('/authenticate/:provider', async function (req, res, next) {
 // })
 
 
-app.use(verifyCSRFToken)
+const qs = require('querystring')
+const axios = require('axios')
 
-app.post('/authenticate', express.json(), function (req, res, next) {
+class OAuth2 {
+  constructor ({
+    clientId,
+    clientSecret,
+    authenticationEndpoint,
+    additionalAuthenticationParameters,
+    scope,
+  }) {
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+    this.authenticationEndpoint = authenticationEndpoint
+    this.additionalAuthenticationParameters = additionalAuthenticationParameters
+    this.scope = scope
+  }
+  getAuthenticationURL (redirectURL, state) {
+    const query = qs.stringify({
+      client_id: this.clientId,
+      response_type: 'code',
+      redirect_uri: redirectURL,
+      scope: this.scope,
+      state: Buffer.from(JSON.stringify(state)).toString('base64'),
+      ...this.additionalAuthenticationParameters
+    })
+
+    return `${this.authenticationEndpoint}?${query}`
+  }
+
+  async handleCallback (code, xsrfToken) {
+
+  }
+
+}
+
+const google = new OAuth2({
+  clientId: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_SECRET,
+  authenticationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  scope: 'profile email',
+  additionalAuthenticationParameters: {
+  }
+})
+const facebook = new OAuth2({
+  clientId: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_SECRET,
+  authenticationEndpoint: 'https://www.facebook.com/v3.2/dialog/oauth',
+  scope: 'email',
+  additionalAuthenticationParameters: {
+    display: 'touch'
+  }
+})
+
+
+app.get('/authenticate/:provider', async function (req, res, next) {
+  const callbackURL = 'http://localhost:9090/api/v1/auth/callback'
+  const state = {
+    xsrfToken : uuid(),
+    clientState: req.query
+  }
+
+  switch (req.params.provider) {
+    case 'google':
+      res.redirect(google.getAuthenticationURL(callbackURL, state))
+      break
+    case 'facebook':
+      res.redirect(facebook.getAuthenticationURL(callbackURL, state))
+      break
+    default:
+      next(new NotFoundError())
+  }
+})
+
+app.get('/callback', express.urlencoded({ extended: false }), async function (req, res, next) {
+  console.log(req.query)
+  console.log(req.body)
+  google.handleCallback(code)
+
+
+  res.redirect('http://localhost:8080/')
+})
+
+app.post('/authenticate', verifyCSRFToken, express.json(), function (req, res, next) {
   const { username, password } = req.body
 
   if (username === 'admin' && password === 'admin') {
--- artemis/src/utils/OAuth.js
+++ artemis/src/utils/OAuth.js
@@ -1,13 +1,54 @@
-class OAuth {
-  constructor (endpoint, {
+const qs = require('querystring')
+const axios = require('axios')
+
+class OAuth2 {
+  constructor ({
     clientId,
     clientSecret,
+    authenticationEndpoint,
+    additionalAuthenticationParameters,
     scope,
-
   }) {
 
   }
+  getAuthenticationURL (redirectURL, xsrfToken, clientState = {}) {
+    const query = qs.stringify({
+      client_id: this.clientId,
+      response_type: 'code',
+      redirect_uri: redirectURL,
+      state: encodeURIComponent(btoa(JSON.stringify({
+        xsrfToken,
+        clientState
+      }))),
+      ...this.additionalAuthenticationParameters
+    })
+
+    return `${this.authenticationEndpoint}?${query}`
+  }
+
+  async handleCallback (code, xsrfToken) {
+
+  }
+
 }
+
+const google = new OAuth2({
+  clientId: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_SECRET,
+  authenticationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  additionalAuthenticationParameters: {
+    scope: 'profile email'
+  }
+})
+const google = new OAuth2({
+  clientId: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_SECRET,
+  authenticationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  additionalAuthenticationParameters: {
+    scope: 'email',
+    display: 'touch'
+  }
+})
 
 class FacebookOAuth extends OAuth {
 
@@ -26,3 +67,171 @@ class GoogleOAuth extends OAuth {
 //    pass any query parameters in state
 // verify response and extract information
 // extract original query parameters from response state, use those on final redirect
+
+
+const jwt = require('jsonwebtoken')
+const request = require('request-promise')
+
+module.exports = async function clientOpenAuthentication ({ provider, redirect_uri, code }, { db }) {
+  let clientAuth = null
+  let clientAccount = null
+
+  try {
+    switch (provider) {
+      case 'facebook':
+        clientAuth = await getOAuthFacebook(redirect_uri, code)
+        break
+      case 'google':
+        clientAuth = await getOAuthGoogle(redirect_uri, code)
+        break
+      default:
+        throw new Error(`Unknow OpenAuth Provider ${provider}`)
+    }
+
+    clientAccount = await loadClientAccount(clientAuth, db)
+    if (clientAccount === null) {
+      clientAccount = await createClientAccount(clientAuth, db)
+    }
+
+    return {
+      account: clientAccount,
+      token: getClientToken(clientAccount)
+    }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+function getClientToken ({ client_id }) {
+  const claims = {
+    'x-hasura-default-role': 'client',
+    'x-hasura-allowed-roles': ['client'],
+    'x-hasura-client-id': client_id
+  }
+  return jwt.sign({ 'x-hasura': claims }, process.env.AUTH_JWT_SECRET)
+}
+
+async function loadClientAccount (clientAuth, db) {
+  const query = `
+    SELECT client_id, name, email, first_name, middle_name, last_name, authentication_provider_name
+    FROM store.client
+    WHERE authentication_provider_name = $1
+    AND authentication_provider_account_id = $2
+  `
+  const { rows: [ client ] } = await db.query(query, [
+    clientAuth.authentication_provider_name,
+    clientAuth.authentication_provider_account_id
+  ])
+
+  return client ? client : null
+}
+
+async function createClientAccount (clientAuth, db) {
+  const query = `
+    INSERT INTO store.client
+      (name, email, first_name, middle_name, last_name, authentication_provider_name, authentication_provider_account_id)
+    VALUES
+      ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING
+      client_id, name, email, first_name, middle_name, last_name, authentication_provider_name
+  `
+
+  // order here must be same as above. Be careful
+  const { rows: [ client ] } = await db.query(query, [
+    clientAuth.name,
+    clientAuth.email,
+    clientAuth.first_name,
+    clientAuth.middle_name,
+    clientAuth.last_name,
+    clientAuth.authentication_provider_name,
+    clientAuth.authentication_provider_account_id
+  ])
+
+  return client
+}
+
+async function getOAuthFacebook (redirect_uri, code) {
+  const { access_token } = await request({
+    method: 'get',
+    uri: 'https://graph.facebook.com/v3.2/oauth/access_token',
+    json: true,
+    qs: {
+      client_id: process.env.OAUTH_PROVIDER_FACEBOOK_CLIENT_ID,
+      client_secret: process.env.OAUTH_PROVIDER_FACEBOOK_CLIENT_SECRET,
+      redirect_uri,
+      code
+    }
+  })
+
+  const response = await request({
+    method: 'get',
+    uri: 'https://graph.facebook.com/me?fields=id&access_token',
+    json: true,
+    qs: {
+      access_token,
+      fields: 'id,name,email,first_name,last_name,middle_name'
+      // fields: 'id,name,email,first_name,last_name,middle_name,name_format,picture,short_name'
+    }
+  })
+
+  const { id, name, email, first_name, middle_name, last_name } = response
+
+  return {
+    authentication_provider_name: 'facebook',
+    authentication_provider_account_id: id,
+    name: name ? name : '',
+    email: email ? email : '',
+    first_name: first_name ? first_name : '',
+    middle_name: middle_name ? middle_name : '',
+    last_name: last_name ? last_name : ''
+  }
+}
+
+async function getOAuthGoogle (redirect_uri, code) {
+  const { access_token } = await request({
+    method: 'post',
+    json: true,
+    uri: 'https://www.googleapis.com/oauth2/v4/token',
+    qs: {
+      grant_type: 'authorization_code',
+      client_id: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_ID,
+      client_secret: process.env.OAUTH_PROVIDER_GOOGLE_CLIENT_SECRET,
+      redirect_uri,
+      code
+    }
+  })
+
+  const response = await request({
+    method: 'get',
+    uri: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token',
+    json: true,
+    qs: {
+      access_token,
+      fields: 'id,name,email,given_name,family_name'
+      // fields: 'id,email,verified_email,name,given_name,family_name,link,picture,locale'
+    }
+  })
+
+  const { id, name, email, given_name, family_name } = response
+  let first_name = '', middle_name = ''
+  if (given_name && given_name.length) {
+    let namesArr = given_name.split(' ')
+    if (namesArr.length) {
+      first_name = namesArr[0]
+      if (namesArr.length >= 2) {
+        middle_name = namesArr.slice(1).join(' ')
+      }
+    }
+  }
+
+  return {
+    authentication_provider_name: 'google',
+    authentication_provider_account_id: id,
+    name: name ? name : '',
+    email: email ? email : '',
+    first_name: first_name ? first_name : '',
+    middle_name: middle_name ? middle_name : '',
+    last_name: family_name ? family_name : ''
+  }
+}
